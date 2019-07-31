f36ade27529ded21d228911cbcf68332d4077834
DomiasPL <dominik-stepuch@wp.pl>
2019-07-31 12:53:55 +0200

    Added Role entity. Created relation between User and Role

--- src/main/java/com/github/DominasPL/AdventureTime/domain/entities/Role.java
+++ src/main/java/com/github/DominasPL/AdventureTime/domain/entities/Role.java
@@ -1,8 +1,24 @@
 package com.github.DominasPL.AdventureTime.domain.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    
+    @Column(nullable = false, unique = true)
+    private String name;
+
 
 }
--- src/main/java/com/github/DominasPL/AdventureTime/domain/entities/User.java
+++ src/main/java/com/github/DominasPL/AdventureTime/domain/entities/User.java
@@ -1,8 +1,18 @@
 package com.github.DominasPL.AdventureTime.domain.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -14,5 +24,10 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @ManyToMany
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles = new ArrayList<>();
 
 }
