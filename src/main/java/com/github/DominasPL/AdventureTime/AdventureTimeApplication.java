fb5472ccea54cb0c2f5fb9dc0f8c56148a65e3f6
DomiasPL <dominik-stepuch@wp.pl>
2019-07-31 12:21:59 +0200

    Created new project via Spring Boot. Added necessary dependencies to start new project

---
+++ src/main/java/com/github/DominasPL/AdventureTime/AdventureTimeApplication.java
@@ -0,0 +1,13 @@
+package com.github.DominasPL.AdventureTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureTimeApplication.class, args);
+	}
+
+}
